1ded0a285a0989abda48dc1ce1dd2f13d9fc4e38
agaham <agahamsc@live.com>
2020-05-21 14:42:52 +0200

    trying to send reset pw links

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3'
+gem 'dotenv-rails'
 
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+# doesn't have to be Heroku, but you get the idea.
+  config.action_mailer.default_url_options = { :host => 'kolenkit.herokuapp.com' }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
